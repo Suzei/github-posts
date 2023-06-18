91176a6d6fafa1ca0937412c3671016ac59c4465
Suzei <odetofack@gmail.com>
2023-06-18 11:09:10 -0300

    solving cr error

---
+++ src/components/IssueCard/styles.ts
@@ -0,0 +1,10 @@
+import { styled } from 'styled-components';
+
+
+export const IssueCardContainer = styled.div`
+ 
+`
+
+export const IssueCardWrapper = styled.div``
+
+export const IssueCardTitle = styled.h3`` 
